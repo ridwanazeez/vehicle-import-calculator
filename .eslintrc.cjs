2f17f79454d0b0666b49b7a2a88b37f33f6b67bb
ridwanazeez <ridwanazeez2000@gmail.com>
2023-06-28 20:54:58 -0400

    update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,12 @@
-/* eslint-env node */
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier'
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
+  rules: {
   }
 }
