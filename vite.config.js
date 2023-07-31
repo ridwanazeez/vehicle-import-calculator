849886ac50e3d4e951e5875673006baa0499b3f1
ridwanazeez <ridwanazeez2000@gmail.com>
2023-07-30 21:49:21 -0400

    trying path fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vehicle-import-calculator/',
   plugins: [
     vue(),
     VitePWA({
