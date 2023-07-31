6c247750a7b6f4af1b66f445ddd8160c940261cb
ridwanazeez <ridwanazeez@users.noreply.github.com>
2023-07-31 01:50:13 +0000

    deploy: 849886ac50e3d4e951e5875673006baa0499b3f1

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vehicle-import-calculator/sw.js', { scope: '/vehicle-import-calculator/' })})}
