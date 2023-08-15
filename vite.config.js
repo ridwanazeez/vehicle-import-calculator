33e188422ae2ae297fb814c60d8c8ad52a15cbfb
Ridwan Azeez <ridwanazeez2000@gmail.com>
2023-08-15 10:14:41 -0400

    update icon paths

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
+      // Generate service worker in dev mode
+      // devOptions: {
+      //   enabled: true,
+      // },
       registerType: "autoUpdate",
       manifest: {
         theme_color: "#00E6AC",
@@ -17,27 +21,26 @@ export default defineConfig({
         display: "standalone",
         scope: "https://ridwanazeez.github.io/vehicle-import-calculator/",
         start_url: "https://ridwanazeez.github.io/vehicle-import-calculator/",
-        name: "Vehicle Import Calculator",
-        short_name: "Vehicle Import Calculator",
+        name: "Vehicle Import Taxes Calculator",
+        short_name: "Vehicle Import Taxes Calculator",
         icons: [
           {
-            src: "/images/icons/icon-192x192.png",
+            src: "/images/vehicle-import-calculator-icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/images/icons/icon-256x256.png",
+            src: "/images/vehicle-import-calculator-icons/icon-256x256.png",
             sizes: "256x256",
             type: "image/png",
-            purpose: "any maskable",
           },
           {
-            src: "/images/icons/icon-384x384.png",
+            src: "/images/vehicle-import-calculator-icons/icon-384x384.png",
             sizes: "384x384",
             type: "image/png",
           },
           {
-            src: "/images/icons/icon-512x512.png",
+            src: "/images/vehicle-import-calculator-icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
